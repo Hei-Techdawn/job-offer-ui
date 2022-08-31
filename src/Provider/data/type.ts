ba0b229c24ab395323ae02bde6789da54e1b3f91
Brindo <hei.brindo.2@gmail.com>
2022-09-01 02:24:48 +0300

    another add

--- src/Provider/data/type.ts
+++ src/Provider/data/type.ts
@@ -7,3 +7,68 @@ export type TGetAll<T> = {
     lastPage: number;
     data: T;
 }
+
+export type Apply = {
+    id?: number,
+    profile?: Profile,
+    salary?: number,
+    date?: string,
+    offer?: Offer,
+    candidate?: Candidate
+}
+
+export type Candidate = {
+    lastName?: string,
+    firstName?: string,
+    email?: string,
+    address?: string
+}
+
+export type Domain = {
+    id?: Number,
+    name?: string,
+    description?: string
+}
+
+export type History = {
+    id?: number,
+    type?: string,
+    date?: string,
+    offre?: Offer,
+    candidate?: Candidate
+}
+
+export type MyUser = {
+    id?: number,
+    lastName?: string,
+    firstName?: string,
+    userName?: string,
+    password?: string,
+    role?: string
+}
+
+export type Offer = {
+    id?: number,
+    ref?: string,
+    name?: string,
+    status?: string,
+    job?: Job,
+    place?: Place,
+    profile?: Profile,
+    domaine?: Domain
+}
+
+export type Place = {
+    id?: number,
+    name?: string
+}
+
+export type Job = {
+    id?: number,
+    name?: string
+}
+
+export type Profile = {
+    id?: number,
+    profile?: string
+}
